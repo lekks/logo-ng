55855cd838b37e6519c3de3a26b5d00214397515
Alexey Dolinenko <lekks@yandex.ru>
2015-02-13 15:30:59 +0300

    Обработка нажатия выбора уровня

--- app/src/main/java/com/ldir/logo/activities/SelectLevelActivity.java
+++ app/src/main/java/com/ldir/logo/activities/SelectLevelActivity.java
@@ -1,14 +1,17 @@
 package com.ldir.logo.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ldir.logo.R;
 import com.ldir.logo.fieldviews.LevelField;
@@ -26,10 +29,21 @@ public class SelectLevelActivity extends Activity {
         final GridView grid = (GridView) findViewById(R.id.levelsGrid);
         grid.setAdapter(adapter);
 
+        grid.setOnItemClickListener(new LevelStarter());
         TextView textView =  (TextView) findViewById(R.id.selectLevel);
         textView.setTypeface(font);
 
     }
+
+    class LevelStarter implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(SelectLevelActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(SelectLevelActivity.this, GameActivity.class);
+            startActivity(intent);
+        }
+    }
+
 
     class LevelsListAdapter extends BaseAdapter {
         @Override
@@ -47,7 +61,7 @@ public class SelectLevelActivity extends Activity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             if(convertView==null) {
-                LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+                LayoutInflater inflater = LayoutInflater.from(SelectLevelActivity.this);
                 convertView = inflater.inflate(R.layout.list_level, parent,false);
             }
             TextView textView =  (TextView) convertView.findViewById(R.id.listLevelItemLabel);
