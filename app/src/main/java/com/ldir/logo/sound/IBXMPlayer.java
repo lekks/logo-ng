ee254655aa7073113b1e6c45b0178c92083bf30c
Alexey Dolinenko <lekks@yandex.ru>
2015-03-03 20:32:00 +0300

    Зацикливание музыки

--- app/src/main/java/com/ldir/logo/sound/IBXMPlayer.java
+++ app/src/main/java/com/ldir/logo/sound/IBXMPlayer.java
@@ -16,11 +16,11 @@ public class IBXMPlayer extends Thread {
     private final static int BIT_RATE = 44100;
     private final static int MIN_BUF_SIZE = BIT_RATE*2;
 	private boolean stopped = false;  // Тут мьютекс не принципиален
-    private Module module;
+    private Module mModule;
 
 	public IBXMPlayer(byte[] data) throws IOException {
 		super();
-        module = new Module( data );
+        mModule = new Module( data );
 		android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
 		start();
 	}
@@ -33,7 +33,7 @@ public class IBXMPlayer extends Thread {
         buffer = new byte[size];
         file.read(buffer);
         file.close();
-        module = new Module( buffer );
+        mModule = new Module( buffer );
         android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
         start();
     }
@@ -41,50 +41,56 @@ public class IBXMPlayer extends Thread {
 	@Override
     public void run() {
 		AudioTrack track = null;
-        IBXM ibxm = new IBXM( module, BIT_RATE );
+        IBXM ibxm = new IBXM(mModule, BIT_RATE );
         int duration = ibxm.calculateSongDuration();
 
-        Log.i("Mod", "Song name: " + module.songName);
+        Log.i("Mod", "Song name: " + mModule.songName);
         Log.i("Mod", "Song duration: " + duration / BIT_RATE);
 
 		try {
 
-            int[] mixBuf = new int[ ibxm.getMixBufferLength() ];
-            short[] outBuf = new short[ mixBuf.length * 2 ];
 
-			int minSize = AudioTrack.getMinBufferSize(BIT_RATE,
-                    AudioFormat.CHANNEL_OUT_STEREO,
-                    AudioFormat.ENCODING_PCM_16BIT);
+                int[] mixBuf = new int[ibxm.getMixBufferLength()];
+                short[] outBuf = new short[mixBuf.length * 2];
+
+                int minSize = AudioTrack.getMinBufferSize(BIT_RATE,
+                        AudioFormat.CHANNEL_OUT_STEREO,
+                        AudioFormat.ENCODING_PCM_16BIT);
 
 
-            if(minSize < MIN_BUF_SIZE) minSize = MIN_BUF_SIZE;
+                if (minSize < MIN_BUF_SIZE) minSize = MIN_BUF_SIZE;
 //            if(minSize < outBuf.length*2) minSize = outBuf.length*2;
 
-            track = new AudioTrack(AudioManager.STREAM_MUSIC, BIT_RATE,
-					AudioFormat.CHANNEL_OUT_STEREO,
-					AudioFormat.ENCODING_PCM_16BIT, minSize,
-					AudioTrack.MODE_STREAM);
-            track.play();
-            while (  duration >0  && !stopped) {
-                int samples = ibxm.getAudio( mixBuf );
-                int outIdx = 0;
-                for( int mixIdx = 0, mixEnd = samples * 2; mixIdx < mixEnd; mixIdx++ ) {
-                    int sam = mixBuf[ mixIdx ];
-                    if( sam > 32767 ) sam = 32767;
-                    if( sam < -32768 ) sam = -32768;
-                    sam /=2; // Громкость
-                    outBuf[ outIdx++ ] = ( short ) sam;
-                }
-                duration -= samples;
-                int wpos = 0;
-                while(wpos<outIdx && !stopped)
-                    wpos+=track.write( outBuf, wpos, outIdx-wpos);
-            }
-            if(duration<=0)
-                Log.i("Mod", "Song ended");
-            if(stopped)
-                Log.i("Mod", "Song stopped");
+                track = new AudioTrack(AudioManager.STREAM_MUSIC, BIT_RATE,
+                        AudioFormat.CHANNEL_OUT_STEREO,
+                        AudioFormat.ENCODING_PCM_16BIT, minSize,
+                        AudioTrack.MODE_STREAM);
+                track.play();
+            while (!stopped) {
+                duration = ibxm.calculateSongDuration();
 
+                while (duration > 0 && !stopped) {
+                    int samples = ibxm.getAudio(mixBuf);
+                    int outIdx = 0;
+                    for (int mixIdx = 0, mixEnd = samples * 2; mixIdx < mixEnd; mixIdx++) {
+                        int sam = mixBuf[mixIdx];
+                        if (sam > 32767) sam = 32767;
+                        if (sam < -32768) sam = -32768;
+                        sam /= 2; // Громкость
+                        outBuf[outIdx++] = (short) sam;
+                    }
+                    duration -= samples;
+                    int wpos = 0;
+                    while (wpos < outIdx && !stopped)
+                        wpos += track.write(outBuf, wpos, outIdx - wpos);
+                }
+                if (duration <= 0) {
+                    ibxm.setSequencePos(0);
+                    Log.i("Mod", "Song ended");
+                }
+                if (stopped)
+                    Log.i("Mod", "Song stopped");
+            }
         } catch (Throwable x) {
             x.printStackTrace();
             throw new RuntimeException();
--- app/src/main/java/com/ldir/logo/sound/Music.java
+++ app/src/main/java/com/ldir/logo/sound/Music.java
@@ -1,7 +1,9 @@
 package com.ldir.logo.sound;
 
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 
+import com.ldir.logo.R;
 import com.ldir.logo.activities.GameApp;
 
 import java.io.IOException;
@@ -14,26 +16,36 @@ public class Music {
 
     private static boolean isON = false;
 
+    private static void close() {
+        if (music != null) {
+            try {
+                music.close();
+                music = null;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                throw new RuntimeException();
+            }
+        }
+    }
+
+    private static void open(String name) {
+        try {
+            music = new IBXMPlayer(GameApp.getAsset(name));
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+
     private static void updade() {
         if(isON && getMusicEnabled()) {
             if (music == null) {
-                try {
-                    music = new IBXMPlayer(GameApp.getAsset("mus/menu/1.xm"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    throw new RuntimeException();
-                }
+                String[] songs = GameApp.getAppResources().getStringArray(R.array.game_mus);
+                open(songs[0]);
             }
         } else {
-            if (music != null) {
-                try {
-                    music.close();
-                    music = null;
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    throw new RuntimeException();
-                }
-            }
+            close();
         }
     }
 
